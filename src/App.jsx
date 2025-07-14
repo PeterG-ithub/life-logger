35e4abad4f71c8809f85a16a54b86c87f7eb61b5
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2025-07-13 19:08:04 -0700

    Change website title
    - Remove vite and react svg
    - Change website title to Life logger
    - Update background color of app

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <div class="flex items-center justify-center h-screen bg-zinc-700">
+      <div class="flex items-center justify-center h-screen bg-zinc-900">
         <h1 class="text-4xl font-semibold text-center italic text-stone-200 animate-bounce">
           hello world;
         </h1>
